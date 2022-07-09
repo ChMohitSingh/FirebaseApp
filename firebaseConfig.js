341ada96a865225d2918ba01a41a9d12d887ab4d
ChMohitSingh <mohit.singh_cs20@gla.ac.in>
2022-07-09 16:25:57 +0530

    Final App

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+ // Import the functions you need from the SDKs you need
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.0/firebase-app.js";
+ // TODO: Add SDKs for Firebase products that you want to use
+ // https://firebase.google.com/docs/web/setup#available-libraries
+
+ // Your web app's Firebase configuration
+ const firebaseConfig = {
+   apiKey: "AIzaSyAngyaP9qmsk-_9u4e8E8xBTlsMdEdfQf8",
+   authDomain: "firewebapp-487eb.firebaseapp.com",
+   projectId: "firewebapp-487eb",
+   storageBucket: "firewebapp-487eb.appspot.com",
+   messagingSenderId: "662571284476",
+   appId: "1:662571284476:web:8f938c1d2822901e466479"
+ };
+
+ // Initialize Firebase
+ const app = initializeApp(firebaseConfig);
